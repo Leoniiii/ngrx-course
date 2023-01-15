507d388005ff369c65f84e0c520a9d635202e85e
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2023-01-15 16:52:25 +0100

    6.47 Implementing the course component using ngrx data

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,10 +1,11 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
-import {Observable} from 'rxjs';
-import {Lesson} from '../model/lesson';
-import {concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom} from 'rxjs/operators';
-import {CoursesHttpService} from '../services/courses-http.service';
+import { CourseEntityService } from './../services/course-entity.service';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../model/course';
+import { Observable, of } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom } from 'rxjs/operators';
+import { CoursesHttpService } from '../services/courses-http.service';
 
 
 @Component({
@@ -23,7 +24,7 @@ export class CourseComponent implements OnInit {
   nextPage = 0;
 
   constructor(
-    private coursesService: CoursesHttpService,
+    private coursesService: CourseEntityService,
     private route: ActivatedRoute) {
 
   }
@@ -32,12 +33,12 @@ export class CourseComponent implements OnInit {
 
     const courseUrl = this.route.snapshot.paramMap.get("courseUrl");
 
-    this.course$ = this.coursesService.findCourseByUrl(courseUrl);
+    this.course$ = this.coursesService.entities$
+      .pipe(
+        map(courses => courses.find(course => course.url == courseUrl))
+      )
 
-    this.lessons$ = this.course$.pipe(
-      concatMap(course => this.coursesService.findLessons(course.id)),
-      tap(console.log)
-    );
+    this.lessons$ = of([])
 
   }
 
